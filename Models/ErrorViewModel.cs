c27bf9706b71d1df1af8b21d83e21c65821241d9
HarramKhan <khanharry786786@gmail.com>
2022-12-09 03:24:43 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Arthub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
